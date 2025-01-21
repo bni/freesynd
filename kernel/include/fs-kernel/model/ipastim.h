4130d26cc6ccdefea3495385a60dfddfd57bdf31
benblan <benblan@users.sourceforge.net>
2025-01-21 21:50:05 +0100

    Update copyright notice in kernel directory

--- kernel/include/fs-kernel/model/ipastim.h
+++ kernel/include/fs-kernel/model/ipastim.h
@@ -1,24 +1,23 @@
-/************************************************************************
- *                                                                      *
- *  FreeSynd - a remake of the classic Bullfrog game "Syndicate".       *
- *                                                                      *
- *   Copyright (C) 2012  Ryan Cocks <ryan@ryancocks.net>                *
- *                                                                      *
- *    This program is free software;  you can redistribute it and / or  *
- *  modify it  under the  terms of the  GNU General  Public License as  *
- *  published by the Free Software Foundation; either version 2 of the  *
- *  License, or (at your option) any later version.                     *
- *                                                                      *
- *    This program is  distributed in the hope that it will be useful,  *
- *  but WITHOUT  ANY WARRANTY;  without even  the implied  warranty of  *
- *  MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
- *  General Public License for more details.                            *
- *                                                                      *
- *    You can view the GNU  General Public License, online, at the GNU  *
- *  project's  web  site;  see <http://www.gnu.org/licenses/gpl.html>.  *
- *  The full text of the license is also included in the file COPYING.  *
- *                                                                      *
- ************************************************************************/
+/*
+ *  FreeSynd - a remake of the classic Bullfrog game "Syndicate".
+ *
+ *   Copyright (C) 2012  Ryan Cocks <ryan@ryancocks.net>
+ *   Copyright (C) 2025  Benoit Blancard <benblan@users.sourceforge.net>
+ *
+ *   This program is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ * 
+ */
 
 #ifndef FreeSynd_ipastim_h
 #define FreeSynd_ipastim_h
