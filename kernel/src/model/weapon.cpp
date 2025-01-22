7527cdbe6502b619929e22557cc7d65e19e64435
benblan <benblan@users.sourceforge.net>
2025-01-22 20:35:18 +0100

    Update copyright notice in kernet directory

--- kernel/src/model/weapon.cpp
+++ kernel/src/model/weapon.cpp
@@ -1,29 +1,28 @@
-/************************************************************************
- *                                                                      *
- *  FreeSynd - a remake of the classic Bullfrog game "Syndicate".       *
- *                                                                      *
- *   Copyright (C) 2005  Stuart Binge  <skbinge@gmail.com>              *
- *   Copyright (C) 2005  Joost Peters  <joostp@users.sourceforge.net>   *
- *   Copyright (C) 2006  Trent Waddington <qg@biodome.org>              *
- *   Copyright (C) 2006  Tarjei Knapstad <tarjei.knapstad@gmail.com>    *
- *   Copyright (C) 2010  Bohdan Stelmakh <chamel@users.sourceforge.net> *
- *   Copyright (C) 2011  Mark <mentor66@users.sourceforge.net>          *
- *                                                                      *
- *    This program is free software;  you can redistribute it and / or  *
- *  modify it  under the  terms of the  GNU General  Public License as  *
- *  published by the Free Software Foundation; either version 2 of the  *
- *  License, or (at your option) any later version.                     *
- *                                                                      *
- *    This program is  distributed in the hope that it will be useful,  *
- *  but WITHOUT  ANY WARRANTY;  without even  the implied  warranty of  *
- *  MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
- *  General Public License for more details.                            *
- *                                                                      *
- *    You can view the GNU  General Public License, online, at the GNU  *
- *  project's  web  site;  see <http://www.gnu.org/licenses/gpl.html>.  *
- *  The full text of the license is also included in the file COPYING.  *
- *                                                                      *
- ************************************************************************/
+/*
+ *  FreeSynd - a remake of the classic Bullfrog game "Syndicate".
+ *
+ *   Copyright (C) 2005  Stuart Binge  <skbinge@gmail.com>
+ *   Copyright (C) 2005  Joost Peters  <joostp@users.sourceforge.net>
+ *   Copyright (C) 2006  Trent Waddington <qg@biodome.org>
+ *   Copyright (C) 2006  Tarjei Knapstad <tarjei.knapstad@gmail.com>
+ *   Copyright (C) 2010  Bohdan Stelmakh <chamel@users.sourceforge.net>
+ *   Copyright (C) 2011  Mark <mentor66@users.sourceforge.net>
+ *   Copyright (C) 2024-2025  Benoit Blancard <benblan@users.sourceforge.net>
+ *
+ *   This program is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ * 
+ */
 
 #include "fs-kernel/model/weapon.h"
 
